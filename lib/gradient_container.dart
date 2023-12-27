b0d263bbbe7535bea8ddc52a9aae775827517dc9
Sarthak <sarthak.00puri@gmail.com>
2023-12-27 20:30:18 +0530

    linked assets to pubspec.yaml

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice/styled_text.dart';
+// import 'package:roll_dice/styled_text.dart';
 
-class GradientContainer extends StatelessWidget{
-  const GradientContainer(this.colorList,{super.key});
+void rollDice(){
+  // ...
+}
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colorList, {super.key});
   final List<Color> colorList;
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: colorList,
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colorList,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            Image.asset('assets/dice-6.png', width: 250),
+            const ElevatedButton(onPressed: rollDice, child: Text('Roll Dice'))
+            ]
+            
             ),
-          ),
-          child: const Center(
-            child: StyledText("Roll Dice"),
-          ),
-        );
+      ),
+    );
   }
 }
-
