f9705c2346035604dfd3a2d6d3e41620da3be47f
Sarthak <sarthak.00puri@gmail.com>
2023-12-28 05:15:54 +0530

    updated UI

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# EnchantedRolls
-Flutter-based project focusing on an application where a dice roll is simulated with the click of a button.
+# CosmicDice
+CosmicDice is a Flutter-based project focusing on an application where a dice roll is simulated with the click of a button. This project is part of my journey in learning and exploring Flutter, showcasing fundamental concepts and skills in app development with this framework.
+
+Getting Involved:
+Feel free to explore the code, and suggestions or feedback are always welcome. If you're also on a journey of learning Flutter, I hope this project provides you with insights and inspiration.
+
+Setup:
+To get started with this project, clone the repository and follow the standard Flutter setup procedures.
--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,50 +1,57 @@
-
 import 'dart:math';
 import 'package:flutter/material.dart';
 
+final randomizer = Random();
 
-final randomizer=Random();
-class DiceRoller extends StatefulWidget{
+class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
 
   @override
-  State<DiceRoller> createState(){
+  State<DiceRoller> createState() {
     return _DiceRollerState();
   }
 }
 
-class _DiceRollerState extends State<DiceRoller>{
+class _DiceRollerState extends State<DiceRoller> {
+  var diceNum = randomizer.nextInt(6) + 1;
 
-  var diceNum=randomizer.nextInt(6)+1;
-
-  void rollDice(){
-    setState(() {
-      diceNum=randomizer.nextInt(6)+1;
-    },);
+  void rollDice() {
+    setState(
+      () {
+        diceNum = randomizer.nextInt(6) + 1;
+      },
+    );
   }
 
   @override
-  Widget build(context){
+  Widget build(context) {
     return Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset('assets/dice-$diceNum.png', width: 250),
-            const SizedBox(height: 25,),
-            ElevatedButton(
-              onPressed: rollDice,
-              
-              style: const ButtonStyle(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Text(
+          'Cosmic Dice',
+          style: TextStyle(fontFamily: 'Hater', fontSize: 55, shadows: [
+            Shadow(color: Colors.deepPurple, offset: Offset(3, 0))
+          ]),
+        ),
+        const SizedBox(height: 30,),
+        Image.asset('assets/dice-$diceNum.png', width: 250),
+        const SizedBox(
+          height: 25,
+        ),
+        ElevatedButton(
+            onPressed: rollDice,
+            style: const ButtonStyle(
                 // padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
+                elevation: MaterialStatePropertyAll(10),
                 animationDuration: Duration(seconds: 2),
                 minimumSize: MaterialStatePropertyAll(Size(125, 50)),
-                backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 201, 237, 36)),
-                foregroundColor: MaterialStatePropertyAll(Colors.black)
-              ),
-              child: const Text(
-                'Roll Dice',
-                style: TextStyle(
-                fontFamily: 'Blatter',
-                fontSize: 22)))
-            ],);
+                backgroundColor:MaterialStatePropertyAll(Color.fromARGB(255, 201, 237, 36)),
+                foregroundColor: MaterialStatePropertyAll(Colors.black),
+                shadowColor: MaterialStatePropertyAll(Colors.deepPurple)),
+            child: const Text('Roll it!',
+                style: TextStyle(fontFamily: 'Blatter', fontSize: 22)))
+      ],
+    );
   }
 }
